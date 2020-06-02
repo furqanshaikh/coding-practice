d3cfaa76654db302997aed44f434f968becddcbf
Furqan Shaikh <fshaikh@outlook.com>
2020-06-02 07:26:28 +0500

    Adds BinaryGap and CyclicRotation solutions

---
+++ src/main/java/codility/BinaryGap.java
@@ -0,0 +1,32 @@
+package codility;
+
+public class BinaryGap {
+
+    public String toBinaryString(int N) {
+        StringBuilder sb = new StringBuilder();
+        while (N > 0) {
+            sb.append(N % 2);
+            N /= 2;
+        }
+        return sb.reverse().toString();
+    }
+
+    public int maxBinaryGap(int N) {
+
+        if (N <= 0) return 0;
+        String binaryStr = toBinaryString(N);
+
+        int maxGap = 0, currentGap = 0;
+
+        for (int i = 0; i < binaryStr.length(); i++) {
+            if (binaryStr.charAt(i) == '0') currentGap++;
+            else if (binaryStr.charAt(i) == '1') {
+                maxGap = Math.max(maxGap, currentGap);
+                currentGap = 0;
+            }
+        }
+
+        return maxGap;
+
+    }
+}
---
+++ src/test/java/codility/BinaryGapTest.java
@@ -0,0 +1,15 @@
+package codility;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;;
+
+class BinaryGapTest {
+
+    @Test
+    void solution() {
+        BinaryGap binaryGap = new BinaryGap();
+        assertEquals(5, binaryGap.maxBinaryGap(1041));
+
+
+    }
+}
