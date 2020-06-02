a4d88c7dd4896c61b368743718a45070aad23ecd
Furqan Shaikh <fshaikh@outlook.com>
2020-06-02 08:35:35 +0500

    Cyclic Rotation using System arraycopy

--- src/main/java/codility/CyclicRotation.java
+++ src/main/java/codility/CyclicRotation.java
@@ -4,65 +4,82 @@ import java.util.Arrays;
 
 /**
  * An array A consisting of N integers is given. Rotation of the array means that each element is shifted right by one index, and the last element of the array is moved to the first place. For example, the rotation of array A = [3, 8, 9, 7, 6] is [6, 3, 8, 9, 7] (elements are shifted right by one index and 6 is moved to the first place).
- *
+ * <p>
  * The goal is to rotate array A K times; that is, each element of A will be shifted to the right K times.
- *
+ * <p>
  * Write a function:
- *
+ * <p>
  * class Solution { public int[] solution(int[] A, int K); }
- *
+ * <p>
  * that, given an array A consisting of N integers and an integer K, returns the array A rotated K times.
- *
+ * <p>
  * For example, given
- *
- *     A = [3, 8, 9, 7, 6]
- *     K = 3
+ * <p>
+ * A = [3, 8, 9, 7, 6]
+ * K = 3
  * the function should return [9, 7, 6, 3, 8]. Three rotations were made:
- *
- *     [3, 8, 9, 7, 6] -> [6, 3, 8, 9, 7]
- *     [6, 3, 8, 9, 7] -> [7, 6, 3, 8, 9]
- *     [7, 6, 3, 8, 9] -> [9, 7, 6, 3, 8]
+ * <p>
+ * [3, 8, 9, 7, 6] -> [6, 3, 8, 9, 7]
+ * [6, 3, 8, 9, 7] -> [7, 6, 3, 8, 9]
+ * [7, 6, 3, 8, 9] -> [9, 7, 6, 3, 8]
  * For another example, given
- *
- *     A = [0, 0, 0]
- *     K = 1
+ * <p>
+ * A = [0, 0, 0]
+ * K = 1
  * the function should return [0, 0, 0]
- *
+ * <p>
  * Given
- *
- *     A = [1, 2, 3, 4]
- *     K = 4
+ * <p>
+ * A = [1, 2, 3, 4]
+ * K = 4
  * the function should return [1, 2, 3, 4]
- *
+ * <p>
  * Assume that:
- *
+ * <p>
  * N and K are integers within the range [0..100];
  * each element of array A is an integer within the range [−1,000..1,000].
  * In your solution, focus on correctness. The performance of your solution will not be the focus of the assessment.
  */
 public class CyclicRotation {
 
+    public int[] rotateKwithArrays(int[] A, int K) {
 
-
-    public int[] rotateK(int[] A, int K){
+        if (K <= 0) return A;
+        if (A.length <= 0) return A;
 
         K = K % A.length;
 
-        for (int i = 0; i < K ;i++) {
+        int[] B = Arrays.copyOfRange(A, A.length - K, A.length);
+        System.arraycopy(A, 0, A, K, A.length - K);
+        System.arraycopy(B, 0, A, 0, B.length);
+
+        return A;
+    }
+
+    public int[] rotateK(int[] A, int K) {
+
+        if (K <= 0) return A;
+        if (A.length <= 0) return A;
+
+        K = K % A.length;
+
+        for (int i = 0; i < K; i++) {
             int lastIndex = A.length - 1;
             int temp = A[lastIndex];
-            for(int j = A.length -1; j >0; j--) {
-                A[j] = A[j-1];
+            for (int j = A.length - 1; j > 0; j--) {
+                A[j] = A[j - 1];
             }
             A[0] = temp;
         }
         return A;
     }
 
-  public static void main(String[] args) {
-        int[] A = {1, 2, 3, 4};
-        int K = 4;
-        int[] solution = new CyclicRotation().rotateK(A, K);
+    public static void main(String[] args) {
+        int K = 2;
+        int[] solutionInts = new CyclicRotation().rotateKwithArrays(new int[]{1, 2, 3, 4}, K);
+        System.out.println(Arrays.deepToString(new int[][]{solutionInts}));
+
+        int[] solution = new CyclicRotation().rotateK(new int[]{1, 2, 3, 4}, K);
         System.out.println(Arrays.deepToString(new int[][]{solution}));
     }
 }
---
+++ src/test/java/codility/CyclicRotationTest.java
@@ -0,0 +1,14 @@
+package codility;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CyclicRotationTest {
+
+    @Test
+    void rotateK() {
+        int[] emptyArray = new int[0];
+        assertEquals(emptyArray, emptyArray);
+    }
+}
